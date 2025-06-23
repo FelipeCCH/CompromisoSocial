c31ca54af4ea5f69360b4c51a8e9e0e963d9072d
Andresdbs04 <andres.benavidessancho@ucr.ac.cr>
2025-06-22 20:23:53 -0600

    agregué un botón en listaVisitante

--- View/ListaVisitante.Designer.cs
+++ View/ListaVisitante.Designer.cs
@@ -31,6 +31,7 @@
             this.datagridListaVisitante = new System.Windows.Forms.DataGridView();
             this.btnCerrar = new System.Windows.Forms.Button();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnRegistrarVisita = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.datagridListaVisitante)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,7 +54,7 @@
             this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCerrar.Font = new System.Drawing.Font("Cooper Black", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCerrar.ForeColor = System.Drawing.Color.White;
-            this.btnCerrar.Location = new System.Drawing.Point(620, 381);
+            this.btnCerrar.Location = new System.Drawing.Point(449, 381);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(120, 36);
             this.btnCerrar.TabIndex = 6;
@@ -70,14 +71,28 @@
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(800, 40);
             this.lblTitulo.TabIndex = 7;
-            this.lblTitulo.Text = "Lista de Visitante";
+            this.lblTitulo.Text = "Listado de Visitas";
             this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRegistrarVisita
+            // 
+            this.btnRegistrarVisita.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnRegistrarVisita.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrarVisita.Font = new System.Drawing.Font("Cooper Black", 10.2F);
+            this.btnRegistrarVisita.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnRegistrarVisita.Location = new System.Drawing.Point(198, 381);
+            this.btnRegistrarVisita.Name = "btnRegistrarVisita";
+            this.btnRegistrarVisita.Size = new System.Drawing.Size(178, 36);
+            this.btnRegistrarVisita.TabIndex = 8;
+            this.btnRegistrarVisita.Text = "Registrar Visita";
+            this.btnRegistrarVisita.UseVisualStyleBackColor = false;
             // 
             // ListaVisitante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegistrarVisita);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.datagridListaVisitante);
@@ -93,5 +108,6 @@
         private System.Windows.Forms.DataGridView datagridListaVisitante;
         private System.Windows.Forms.Button btnCerrar;
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnRegistrarVisita;
     }
 }
